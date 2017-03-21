a97d2f534f2cee5d51846bb4256e601f84a73ffb
varun-kamath <varun.kamath@yahoo.com>
2017-03-21 15:20:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-import urllib
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
 
@@ -19,49 +26,35 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    print(res)
+    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def makeWebhookResult(req):
+
+def processRequest(req):
     if req.get("result").get("action") != "search.components":
         return {}
-    else
-        speech = "test"
-    """
-    result = req.get("result")
-    parameters = result.get("parameters")
-    components = parameters.get("Components")
-    number = parameters.get("Number")
-
-    stock = {'Beaglebone':0, 'Raspberry Pi':2, 'Arduino Uno':4}
-
-    if number >= stock[component]:
-        speech = "Yes we have " + number + " " + components + " in stock. Do you want to place a request?"
-    elif number > 0
-        speech = "We only have " + str(stock[components]) + " " + components + " in stock. Do you want to place a request for the same?"
-    else
-        speech = "I'm sorry we do are out of " + components + ". Contact Lab Admin for Further Queries"
-"""
+    speech = "test"
+    
     print("Response:")
     print(speech)
 
     return {
         "speech": speech,
         "displayText": speech,
-        #"data": {},
+        # "data": data,
         # "contextOut": [],
-        "source": "varun-kamath/tqb-sparkbot"
+        "source": "apiai-weather-webhook-sample"
     }
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+    print("Starting app on port %d" % port)
 
-    app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
